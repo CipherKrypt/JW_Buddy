782ca0d279e54a22af3956912a790053e8b712d9
CipherKrypt <SS4334@live.mdx.ac.uk>
2024-01-09 08:57:58 +0400

    Testing vercel

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import TBot
 import Bible
 from DBR import *
 
-# keep_alive()
-# TBot.bot()
+keep_alive()
+TBot.bot()
 
-AI = DBR_AI(fileName="DBR_Schedule")
-# AI.generate_schedule()
-p = AI[get_day()]
-print(p)
+# AI = DBR_AI(fileName="DBR_Schedule")
+# # AI.generate_schedule()
+# p = AI[get_day()]
+# print(p)
